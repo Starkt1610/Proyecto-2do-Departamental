cd840132257595837adf66523c24ce6986d32b3b
Starkt1610 <ianstark997@gmail.com>
2025-10-30 20:02:41 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Menu from './components/Menu'
 import Carrito from './components/Carrito'
 import Usuario from './components/Usuario'
